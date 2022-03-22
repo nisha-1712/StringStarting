9c6be1860feeef2163d2e3146bc8f9b7a2910269
Nisha Bhavsar <101039209+nisha-1712@users.noreply.github.com>
2022-03-22 17:03:22 +0530

    New file added

---
+++ src/Sd/S.java
@@ -0,0 +1,11 @@
+package Sd;
+
+public class S {
+    public static void main (String []args) {
+        String a="vasundhara";
+        System.out.println(a);
+
+        String b="vasundhara";
+        System.out.println(b);
+    }
+}
